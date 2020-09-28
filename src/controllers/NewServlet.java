a5d4a8e69dec524ae8798d0c51f21d6a4a3319a8
daiki.asayama <daiki.asayama@g.softbank.co.jp>
2020-09-28 18:20:52 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.tasks;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -27,8 +32,27 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
+        // tasksのインスタンスを生成
+        tasks m = new tasks();
+
+        // mの各プロパティにデータを代入
+        String content = "hello";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(m);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+
+        em.close();
+     }
 }
